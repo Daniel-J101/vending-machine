5efd3c02824657824425b8d45fcd0068723d1414
nandji <frostysausage123@gmail.com>
2022-01-28 19:25:18 -0600

    Machine class

--- vending/src/Item.java
+++ vending/src/Item.java
@@ -20,4 +20,27 @@ public class Item {
         return name + ": " + price + " (" + quantity + ")";
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- vending/src/Machine.java
+++ vending/src/Machine.java
@@ -1,6 +1,13 @@
 public class Machine {
+    Item[][] items = new Item[3][9];
 
-
+    public Machine(Item[][] items) {
+        for(int i = 0; i < items.length; i++) {
+            for(int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = items[i][j];
+            }
+        }
+    }
 
     /**
      * Function name – dispense
@@ -13,4 +20,20 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int spot) {
+        int quantity = items[row][spot].getQuantity();
+        if (quantity > 0) {
+            items[row][spot].setQuantity(quantity - 1);
+            return true;
+        } else
+            return false;
+    }
+
+    public Item getItem(int row, int spot)  {
+        return new Item(items[row][spot]);
+    }
+
+    public void setItem(Item item, int row, int spot) {
+        items[row][spot] = new Item(item);
+    }
 }
